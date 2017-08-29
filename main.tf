a38b8e4c1316c8f6b681755300b67fe185d0d1c5
timchen <cjh0313@163.com>
2017-08-29 15:35:59 +0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  region = "${var.region}"
+  project = "${var.project_name}"
+  credentials = "${file(var.account_file_path)}"
+}
+
+resource "google_compute_instance" "ctl-test-instance" {
+  count = 2
+  name = "ctl-test-instance-${count.index}"
+  machine_type = "n1-standard-1"
+  zone = "${var.region_zone}"
+  tags = ["ctl-test-node"]
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20170815a"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+        # Ephemeral
+    }
+  }
+  service_account {
+    scopes = [
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring.write",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/trace.append"
+      ]
+  }
+
+  metadata_startup_script = <<SCRIPT
+echo helloworld ctl > test.txt
+SCRIPT
+
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+region = "us-central1"
+region_zone = "us-central1-a"
+project_name = "my-initial-project-175007"
+account_file_path = "my_initial_project.json"
+
+
